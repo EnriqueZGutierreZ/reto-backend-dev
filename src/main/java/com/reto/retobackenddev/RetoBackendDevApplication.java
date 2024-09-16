05ae4ea0391211f1c526531396abd77b733594e0
Jose Enrique Gutierrez Arias <gutierrez.enrique.ja@gmail.com>
2024-09-15 21:17:14 -0500

    prueba

---
+++ src/main/java/com/reto/retobackenddev/RetoBackendDevApplication.java
@@ -0,0 +1,13 @@
+package com.reto.retobackenddev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoBackendDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetoBackendDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reto/retobackenddev/RetoBackendDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.retobackenddev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoBackendDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
